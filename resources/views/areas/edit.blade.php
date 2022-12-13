ab118f89637b3dc649be1b9007913df64909d7f2
ngcruz <ngcruz@sinci.com>
2022-12-13 14:27:35 -0600

    #Crud Areas

---
+++ resources/views/areas/edit.blade.php
@@ -0,0 +1,49 @@
+ <!--Inicio del modal actualizar-->
+ <div class="modal fade" id="myModalEdit{{$var['id']}}" aria-labelledby="myModal"  aria-hidden="true">
+                                <div class="modal-dialog modal-primary modal-lg" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h4 class="modal-title" style="color:rgb(0,51,100)">Actualizar Parte</h4>
+                                            <button type="button" class="close"  data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">×</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <form action="{{route('ar_edit')}}" role="form" method="post"  class="form-horizontal">
+                                            {{ csrf_field() }} {{method_field('PUT')}}
+                                                <div class="form-group row">
+                                                    <label class="col-md-3 form-control-label" for="text-input">Área</label>
+                                                    <div class="col-md-9">
+                                                        <input type="hidden" class="form-control" name="id"  value="{{$var['id']}}" required> 
+                                                        <input type="text" class="form-control" name="name" placeholder="Nombre del Área" value="{{$var['namearea']}}" maxlength="30" required>
+                                                    </div>
+                                                </div>
+                                                <div class="form-group row">
+                                                    <label class="col-md-3 form-control-label" for="text-input">Posicición</label>
+                                                    <div class="col-md-9">
+                                                        <input type="number" class="form-control" name="pos" placeholder="Posición" value="{{$var['pos']}}" maxlength="30" required>
+                                                    </div>
+                                                </div>
+                                                <div class="form-group row">
+                                                    <label class="col-md-3 form-control-label" for="text-input">Línea</label>
+                                                    <div class="col-md-9">
+                                                        <select class="form-control" name="idmachine" required>
+                                                            <option value="">Seleccione</option>
+                                                            @foreach($machines as $machine)
+                                                            <option value="{{$machine['id']}}"  @if($var['idmachines']===$machine['id']) selected='selected' @endif>{{$machine['name']}} </option>
+                                                            @endforeach
+                                                        </select>
+                                                    </div>
+                                                </div>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-success" data-dismiss="modal">Cerrar</button>
+                                            <input type="submit" class="btn btn-success" value="Actualizar">
+                                            </form>
+                                        </div>
+                                    </div>
+                                    <!-- /.modal-content -->
+                                </div>
+                                <!-- /.modal-dialog -->
+                            </div>
+                            <!--Fin del modal-->
